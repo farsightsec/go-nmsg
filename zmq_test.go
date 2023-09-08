c06ad8a4fa11be97033ceeb7f5d98cc66ed20e1f
Demian Vladi <dvladi@domaintools.com>
2023-09-08 00:43:05 -0700

    Removed time delay and unbind

--- zmq_test.go
+++ zmq_test.go
@@ -5,9 +5,7 @@ import (
 	"github.com/farsightsec/go-nmsg"
 	"io"
 	"reflect"
-	"strings"
 	"testing"
-	"time"
 )
 
 func PayloadIsEqual(c *nmsg.NmsgPayload, d *nmsg.NmsgPayload) bool {
@@ -56,12 +54,6 @@ func doTestDo(t *testing.T, i nmsg.Input, o nmsg.Output) {
 	}
 }
 
-func doUnbind(o io.Writer) error {
-	if obj, ok := o.(interface{ Unbind() error }); ok {
-		return obj.Unbind()
-	}
-	return nil
-}
 func doTestUnbuffered(t *testing.T, r io.Reader, w io.Writer) {
 	input := nmsg.NewInput(r, 1000)
 	output := nmsg.UnbufferedOutput(w)
@@ -94,25 +86,20 @@ func doTestFor(t *testing.T, ep string, tp string, fn tester) {
 	}
 
 	fn(t, reader, writer)
-	if strings.HasPrefix(ep, "tcp") || strings.HasPrefix(ep, "inproc") {
-		doUnbind(writer)
-		// Have to sleep to allow ZMQ unbind to finish for TCP
-		time.Sleep(1000 * 1000 * 10)
-	}
 }
 
 func TestZMQLocal(t *testing.T) {
 	doTestFor(t, "tcp://127.0.0.1:5555", "pushpull", doTestUnbuffered)
-	doTestFor(t, "tcp://127.0.0.1:5555", "pushpull", doTestBuffered)
-	doTestFor(t, "tcp://127.0.0.1:5556", "pubsub", doTestUnbuffered)
-	doTestFor(t, "tcp://127.0.0.1:5556", "pubsub", doTestBuffered)
+	doTestFor(t, "tcp://127.0.0.1:5556", "pushpull", doTestBuffered)
+	doTestFor(t, "tcp://127.0.0.1:5557", "pubsub", doTestUnbuffered)
+	doTestFor(t, "tcp://127.0.0.1:5558", "pubsub", doTestBuffered)
 }
 
 func TestZMQInproc(t *testing.T) {
 	doTestFor(t, "inproc://TestZMQInproc1", "pushpull", doTestUnbuffered)
-	doTestFor(t, "inproc://TestZMQInproc1", "pushpull", doTestBuffered)
-	doTestFor(t, "inproc://TestZMQInproc2", "pubsub", doTestUnbuffered)
-	doTestFor(t, "inproc://TestZMQInproc2", "pubsub", doTestBuffered)
+	doTestFor(t, "inproc://TestZMQInproc2", "pushpull", doTestBuffered)
+	doTestFor(t, "inproc://TestZMQInproc3", "pubsub", doTestUnbuffered)
+	doTestFor(t, "inproc://TestZMQInproc4", "pubsub", doTestBuffered)
 }
 
 func TestZMQIpc(t *testing.T) {
